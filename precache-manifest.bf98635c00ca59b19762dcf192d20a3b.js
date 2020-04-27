6b6c916b0c2f6d7bfb960b28569ad1db7b0fb8ae
Catalin <lungancatalin@yahoo.com>
2020-04-27 21:34:23 +0300

    Updates

---
+++ precache-manifest.bf98635c00ca59b19762dcf192d20a3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3750bcce9a1b96524e2a45a944205df1",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "876452bbed677e3237a3",
+    "url": "/cards/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "64b78e4de8e996eeef58",
+    "url": "/cards/static/js/2.0ae6c2c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cards/static/js/2.0ae6c2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "876452bbed677e3237a3",
+    "url": "/cards/static/js/main.35fb1d24.chunk.js"
+  },
+  {
+    "revision": "f2a8960eb2c0ef54ca9e",
+    "url": "/cards/static/js/runtime-main.a9713abb.js"
+  },
+  {
+    "revision": "8e4a5574d019096b62a948e62e920308",
+    "url": "/cards/static/media/icon-calculator.8e4a5574.svg"
+  },
+  {
+    "revision": "ae2dc13dd8be5d72dc6ea67236f8729c",
+    "url": "/cards/static/media/icon-karma.ae2dc13d.svg"
+  },
+  {
+    "revision": "8f7824b371e618577b20ba3775d9cfb4",
+    "url": "/cards/static/media/icon-supervisor.8f7824b3.svg"
+  },
+  {
+    "revision": "8a1a6a173b407f9e45ba99fb7f25f0db",
+    "url": "/cards/static/media/icon-team-builder.8a1a6a17.svg"
+  }
+]);
